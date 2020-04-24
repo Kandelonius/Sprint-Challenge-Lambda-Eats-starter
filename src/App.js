a22c3b3c618e5efd8e5984c1c0ee9d9ac8b43507
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-24 08:41:12 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom'
 import axios from 'axios'
 import * as yup from 'yup'
 
 const App = () => {
+  const [] = useState([]);
+
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <h1>Pizza Pizza</h1>
+      <Switch>
+        {/* <Route path='/pizza/:id' component={Pizza} />
+        <Route path="/" component={} /> */}
+      </Switch>
+    </div>
   );
 };
+
 export default App;
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Form(props) {
     const {
-        values, 
-        onInputChange, 
+        values,
+        onInputChange,
         onCheckboxChange,
         onSubmit,
+        disabled,
         errors,
     } = props
     // debugger
@@ -24,7 +25,17 @@ function Form(props) {
                     name='username'
                     type='text'
                 /></label><br />
-            <label>Terms of service:&nbsp;
+            <label>Size:&nbsp;
+      <select
+                    name='size'
+                    value={values.size}
+                    onChange={onInputChange}
+                >
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='large'>large</option>
+                </select></label>
+            <label>Toppings:&nbsp;
                 <input
                     checked={values.topping}
                     onChange={onCheckboxChange}
